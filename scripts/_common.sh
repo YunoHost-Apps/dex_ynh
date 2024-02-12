3b38721ae0e1f595d7e330b10aeb25450e760a48
OniriCorpe <oniricorpe@disroot.org>
2024-02-12 23:43:47 +0100

    extract docker image

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,31 @@
 # COMMON VARIABLES
 #=================================================
 
-GO_VERSION="1.20"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+_download_vaultwarden_from_docker() {
+    docker_image="dexidp/dex"
+    debian=$(lsb_release --codename --short)
+    if [[ $debian = "bullseye" ]]; then
+        docker_version="$(ynh_app_upstream_version)-distroless"
+    elif [[ $debian = "bookworm" ]]; then
+        docker_version="$(ynh_app_upstream_version)-alpine"
+    fi
+
+    docker_arg=""
+    # Fixup for armhf
+    if [ "$YNH_ARCH" == "armhf" ]; then
+        docker_arg="--os_arch_variant=linux/arm/v7"
+    fi
+
+    ynh_docker_image_extract --dest_dir="$install_dir" --image_spec="$docker_image:$docker_version" $docker_arg
+
+    chmod 750 "$install_dir"
+    chmod -R o-rwx "$install_dir"
+    chown -R "$app:$app" "$install_dir"
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
