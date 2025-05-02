0395f65f3a9419d13241dedd0ec538095b2b82a5
tituspijean <tituspijean@yunohost.org>
2025-05-02 11:53:02 +0200

    Fix scripts

--- sources/add_config.sh
+++ sources/add_config.sh
@@ -5,27 +5,18 @@
 # $3 is OIDC_CALLBACK
 # $4 is OIDC_SECRET
 
-ynh_string_random() {
-    # Declare an array to define the options of this helper.
-    local legacy_args=lf
-    local -A args_array=([l]=length= [f]=filter=)
-    local length
-    local filter
-    # Manage arguments with getopts
-    ynh_handle_getopts_args "$@"
-    length=${length:-24}
-    filter=${filter:-'A-Za-z0-9'}
-
+simple_random_string() {
     dd if=/dev/urandom bs=1 count=1000 2> /dev/null \
-        | tr --complement --delete "$filter" \
-        | sed --quiet 's/\(.\{'"$length"'\}\).*/\1/p'
+        | tr --complement --delete "A-F0-9" \
+        | sed --quiet 's/\(.\{32\}\).*/\1/p'
 }
 
 if [ -z "$4" ]; then
-    set 4 "$(ynh_string_random --length=32 --filter='A-F0-9')"
+    set 4 "$(simple_random_string)"
 fi
 
 cat <<EOF >> __INSTALL_DIR__/config.yaml.d/$1
+
 - id: $2
   redirectURIs:
   - https://$3
